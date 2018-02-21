40024125aa933e2532916c0a4ad5da1a5586f7d2
kojaniydracon <kojaniydracon@gmail.com>
2018-02-22 01:19:57 +0300

    lovers

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var count = 0;
+    var lover1, lover2, lover3;
+    var arr = {};
+    for (var i = 0; i < preferences.length; i++) {
+        lover1 = preferences[i];
+        lover2 = preferences[lover1 - 1];
+        lover3 = preferences[lover2 -1];
+
+        if (lover1 === preferences[lover3 - 1] && lover1 !== lover3){
+            var key = [lover1, lover2, lover3].sort();
+            arr[key] = true;
+        }
+    }
+    console.log(arr);
+    for (var key in arr) {
+        count++;
+    }
+    return count;
 };
